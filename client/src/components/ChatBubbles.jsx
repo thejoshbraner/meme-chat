fa38e542de196a79b920369496c56a667a0d4319
Josh Braner <braner.josh@gmail.com>
2023-03-21 17:53:31 -0500

    tweaked login... this will either make everything work or completely break it.

--- client/src/components/ChatBubbles.jsx
+++ client/src/components/ChatBubbles.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const OtherBubble = ({ mess }) => {
     return (
-        <div className="min-h-content p-2 mb-1 break-words bg-slate-500 border-2 border-solid border-slate-600 rounded-l text-white px-2 flex items-center self-start">
+        <div className="min-h-content w-full md:w-fit p-2 mb-1 break-words bg-slate-500 border-2 border-solid border-slate-600 rounded-l text-white px-2 flex items-center self-start">
             <span className="text-slate-300 pr-2 font-bold">{mess.username}:</span>
             {mess.message}
         </div>
@@ -11,8 +11,9 @@ export const OtherBubble = ({ mess }) => {
 
 export const SelfBubble = ({ mess }) => {
     return (
-        <div className="min-h-content p-2 mb-1 break-words bg-slate-400 border-2 border-solid border-slate-600 rounded-l text-white px-2 flex items-center self-end">
+        <div className="min-h-content w-full md:w-fit p-2 mb-1 break-words bg-slate-400 border-2 border-solid border-slate-600 rounded-l text-white px-2 flex items-center self-end">
             <span className="text-slate-300 pr-2 font-bold">{mess.username}:</span>
+
             {mess.message}
         </div>
     );
--- client/src/components/LoginRegister.jsx
+++ client/src/components/LoginRegister.jsx
@@ -12,7 +12,7 @@ export const Login = (props) => {
             const response = await fetch("/api/users/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(values),
+                body: JSON.stringify({ username: values.username.toLowercase(), password: values.password }),
             });
             const data = await response.json();
             if (!data.success) {
